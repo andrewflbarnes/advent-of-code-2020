b2992bfd617360bea24573c90f86db72fcf4204e
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2020-12-05 10:55:17 +0000

    Add challenge 5 solution in JS

---
+++ 5/README.md
@@ -0,0 +1,24 @@
+# Challenge 2
+
+The challenge can be found [here][1]
+
+The `input` is a set of seat locations on a plane in which people are currently seated. This is a binary
+based system with a 7-3 split for row-col (see challenge for details).
+
+### Tasks
+
+1. Determine what the highest seat ID which exists
+2. Determine which seat is my seat - not in first/last row, empty (i.e. not in input) and seats either
+side are filled
+
+### Solutions
+
+##### 1 and 2: solution.js
+
+As task 2 criteria are a superset of task 1 criteria there is a single solution.
+```bash
+# Task 1
+./solution.js
+```
+
+[1]: <https://adventofcode.com/2020/day/5> "Advent of Code day 5 challenge"
---
+++ 5/solution.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+
+const debug = process.env.DEBUG == 1 || false
+
+fs.readFile(path.join(__dirname, '/input'), 'utf8', (err, data) => {
+    const lines = data.split('\n')
+    let max = 0
+    let seated = []
+    let possibleSeats = []
+
+    for (const line of lines) {
+      const binRow = line
+        .slice(0, 7)
+        .replace(/B/g, '1')
+        .replace(/F/g, '0')
+      const binCol = line
+        .slice(7)
+        .replace(/R/g, '1')
+        .replace(/L/g, '0')
+
+      const row = parseBin(binRow)
+      const col = parseBin(binCol)
+      const seatId = row * 8 + col
+      debug && console.log(`${seatId}: ${row}, ${col}`)
+
+      seated.push(seatId)
+
+      max = seatId > max ? seatId : max
+    }
+
+    console.log(`Highest seat ID: ${max}`)
+
+    let preSeat
+    let maybeMySeat = seated.includes(8)
+    let postSeat = seated.includes(9)
+
+    const uLimit = 127 * 8
+    for (i = 10; i < uLimit; i++) {
+      preSeat = maybeMySeat
+      maybeMySeat = postSeat
+      postSeat = seated.includes(i)
+
+      if (preSeat && postSeat && !maybeMySeat) {
+        possibleSeats.push(i - 1)
+      }
+    }
+
+    console.log(`Possible seats: ${possibleSeats}`)
+})
+
+function parseBin(str) {
+  let res = 0
+  const strLen = str.length
+  for (i = 0; i < strLen; i++) {
+    if (str.charAt((strLen - i - 1)) === '1') {
+      res += 1 << i
+    }
+  }
+
+  return res
+}
