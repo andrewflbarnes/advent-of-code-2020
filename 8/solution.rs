d0ccb8ff8b5f09c0ba400e95a9e411e16bf0971f
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2020-12-08 12:03:06 +0000

    Added day 8 task 2 solution
    - Used state struct to track processing

--- 8/solution.rs
+++ 8/solution.rs
@@ -2,11 +2,12 @@ use std::env;
 use std::fs;
 use std::str::FromStr;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum OpCode {
     Acc,
     Jmp,
     Noop,
+    Done,
 }
 
 impl FromStr for OpCode {
@@ -16,12 +17,13 @@ impl FromStr for OpCode {
             "acc" => Ok(OpCode::Acc),
             "jmp" => Ok(OpCode::Jmp),
             "nop" => Ok(OpCode::Noop),
+            "done" => Ok(OpCode::Done),
             _ => Err(Self::Err::from(format!("Invalid opcode: {}", s)))
         }
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Op {
     op: OpCode,
     operand: i32,
@@ -41,6 +43,29 @@ impl FromStr for Op {
     }
 }
 
+struct State {
+    acc: i32,
+    idx: usize,
+    complete: bool,
+}
+
+impl State {
+    fn new() -> State {
+        State{
+            acc: 0,
+            idx: 0,
+            complete: false,
+        }
+    }
+
+    fn execute(&mut self, op: &mut Op) {
+        match op.op {
+            OpCode::Done => self.complete = true,
+            _ => self.idx = op.execute(&mut self.acc, self.idx as i32) as usize
+        }
+    }
+}
+
 impl Op {
     fn execute(&mut self, acc: &mut i32, idx: i32) -> i32 {
         self.executions += 1;
@@ -56,6 +81,9 @@ impl Op {
             OpCode::Noop => {
                 return idx + 1;
             },
+            OpCode::Done => {
+                return idx;
+            }
         }
     }
 }
@@ -64,29 +92,85 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
 
-    let mut ops: Vec<Op> = vec![];
+    let mut ref_ops: Vec<Op> = vec![];
     
     fs::read_to_string(filename)
         .expect(&format!("Could not open file: {}", filename))
         .lines()
         .for_each(|line| {
             let op = line.parse::<Op>().unwrap();
-            ops.push(op);
+            ref_ops.push(op);
         });
+
+    ref_ops.push("done 0".parse::<Op>().unwrap());
+    let mut ops = ref_ops.clone();
         
     // println!("{:?}", ops);
 
-    let mut idx = 0;
-    let mut acc = 0;
+    // task 1
+    let mut state = State::new();
     loop {
-        let op = &mut ops[idx];
+        let op = &mut ops[state.idx];
 
         if op.executions > 0 {
+            println!("Program begins looping at index {} with accumulator {}", state.idx, state.acc);
             break;
         }
 
-        idx = op.execute(&mut acc, idx as i32) as usize;
+        state.execute(op);
+
+        if state.complete {
+            println!("UNEXPECTED: Program completed executing at index {} with accumulator {}", state.idx, state.acc);
+            break;
+        }
     }
 
-    println!("Program begins looping at index {} with accumulator {}", idx, acc);
+    //task 2
+    let mut mut_idx = 0;
+    loop {
+        state.idx = 0;
+        state.acc = 0;
+        state.complete = false;
+        let mut mut_ops = ref_ops.clone();
+        let desc;
+
+        loop {
+            let op = &mut mut_ops[mut_idx];
+            mut_idx += 1;
+
+            match op.op {
+                OpCode::Jmp => {
+                    desc = format!("instruction {} jmp->noop", mut_idx - 1);
+                    op.op = OpCode::Noop;
+                    break;
+                },
+                OpCode::Noop => {
+                    desc = format!("instruction {} noop->jmp", mut_idx - 1);
+                    op.op = OpCode::Jmp;
+                    break;
+                },
+                _ => {},
+            }
+        }
+
+        loop {
+            let op = &mut mut_ops[state.idx];
+
+            if op.executions > 0 {
+                // println!("FAILURE: Program begins looping at index {} with accumulator {} (changed {})", state.idx, state.acc, desc);
+                break;
+            }
+
+            state.execute(op);
+
+            if state.complete {
+                break;
+            }
+        }
+
+        if state.complete {
+            println!("Program completed executing at index {} with accumulator {} (changed {})", state.idx, state.acc, desc);
+            break;
+        }
+    }
 }
