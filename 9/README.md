99a7ae6294e06ce1cd4c16494667737d1988c8c3
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2020-12-09 13:20:11 +0000

    Add solution for day 9 challenge task 2 in rust

--- 9/README.md
+++ 9/README.md
@@ -9,7 +9,8 @@ numbers in the previous 25.
 
 1. Based on the input which number does not follow the rule of being composed of 2 numbers from the 25
 preceding it
-2. ???
+2. Find a contiguous set of numbers in the input which sum to the answer from task 1 and find the sum of
+the min and max values from this set
 
 ### Solutions
 
@@ -18,6 +19,13 @@ preceding it
 ```bash
 rustc solution.rs
 ./solution input
+```
+
+##### 2
+
+```bash
+rustc solution2.rs
+./solution2 input
 ```
 
 [1]: <https://adventofcode.com/2020/day/9> "Advent of Code day 9 challenge"
---
+++ 9/solution2.rs
@@ -0,0 +1,66 @@
+use std::env;
+use std::fs;
+use std::io::{
+    BufReader,
+    BufRead
+};
+
+fn main() -> Result<(), std::io::Error> {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    
+    // let target = &args[2].parse::<i64>().unwrap();
+    let target = 217430975;
+    println!("target: {}", target);
+
+    let file = fs::File::open(filename)?;
+    let mut buf = BufReader::new(file);
+    let mut line = String::new();
+    let mut val_buffer: Vec<i64> = vec![];
+    let mut total = 0;
+
+    loop {
+        // println!("entries={}, idx={} buffer_idx={} buffer={:?}", entries, idx, buffer_idx, val_buffer);
+        line.clear();
+
+        match buf.read_line(&mut line) {
+            Ok(bytes_read) => {
+
+                if bytes_read == 0 {
+                    // EOF
+                    break;
+                }
+
+                // remove newline
+                let this_val = line.trim().parse::<i64>().unwrap();
+                val_buffer.push(this_val);
+
+                total += this_val;
+
+                let mut reduction_count = 0;
+                while total > target {
+                    total -= val_buffer[reduction_count];
+                    reduction_count += 1;
+                }
+
+                if reduction_count > 0 {
+                    val_buffer = val_buffer[reduction_count..].to_vec();
+                }
+
+                if total == target {
+                    let max = val_buffer.iter().max().unwrap();
+                    let min = val_buffer.iter().min().unwrap();
+                    println!("Contiguous set summing to {} found with min={}, max={} summing to {}", target, min, max, min + max);
+                    break;
+                }
+
+
+            }
+            Err(err) => {
+                return Err(err);
+            }
+        }
+    }
+
+    Ok(())
+}
