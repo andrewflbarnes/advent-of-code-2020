0e0b631c7aa346e6a42432cc75d89a5ebb8832e7
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2020-12-13 13:13:26 +0000

    Add day 13 challenge task 2 solution in rust
    - What a freaking nightmare - will push a readme explaining what is
      going on later...

--- 13/solution.rs
+++ 13/solution.rs
@@ -11,17 +11,17 @@ fn main() {
         .map(String::from)
         .collect();
 
-    let departure = lines[0].parse::<i32>().unwrap();
-    let buses: Vec<i32> = lines[1]
+    let departure = lines[0].parse::<u64>().unwrap();
+    let buses: Vec<(usize, u64)> = lines[1]
         .split(",")
-        .filter(|bus| bus != &"x")
-        .map(|bus| bus.parse::<i32>().unwrap())
+        .enumerate()
+        .filter(|(_, bus)| bus != &"x")
+        .map(|(i, bus)| (i, bus.parse::<u64>().unwrap()))
         .collect();
-    
-        println!("{} - {:?}", departure, buses);
 
-    let earliest: (i32, i32) = buses.iter()
-        .map(|bus| {
+    // task 1
+    let (earliest_bus, earliest_wait) = buses.iter()
+        .map(|(_, bus)| {
             let wait_time = bus - (departure % bus);
             (bus, wait_time)
         })
@@ -32,6 +32,43 @@ fn main() {
                 acc
             }
         });
+    
+    println!("Earliest bus {} after {} minutes with product {}", earliest_bus, earliest_wait, earliest_bus * earliest_wait);
 
-    println!("{:?} : {}", earliest, earliest.0 * earliest.1);
+    // task 2
+    let (_, first) = buses.first().unwrap();
+
+    // Calculate the modular value per prime bus (excluding first bus)
+    let buses: Vec<(u64, u64)> = buses[1..].iter()
+        .map(|(bus_offset, bus_time)| {
+            let mut modular = 0;
+            loop {
+                modular += 1;
+                if ((first * modular) + *bus_offset as u64) % bus_time == 0 {
+                    return (*bus_time, modular);
+                }
+            }
+        })
+        .collect();
+
+    // println!("Modular values: {:?}", buses);
+    
+    // Calculate the minimum timestamp required
+    let (total_mod, _) = buses.iter()
+        .fold((0u64, 0u64), |(mut total_mod, increment), (bus_time, bus_mod)| {
+            if total_mod == 0 {
+                return (*bus_mod, *bus_time);
+            }
+
+            loop {
+                total_mod += increment;
+                if total_mod % bus_time == *bus_mod {
+                    break;
+                }
+            }
+
+            (total_mod, increment * bus_time)
+        });
+
+    println!("Earliest timestamp where all buses arrive at their offset: {}", first * total_mod);
 }
