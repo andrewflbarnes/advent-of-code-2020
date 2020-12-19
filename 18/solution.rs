261cc2f2ac17f59bafd424d458757362fda925a1
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2020-12-19 06:13:14 +0000

    Updated day 18 challenge to use a single solution
    - Made task 2 precedence configurable and ensure the current stack is
      fully processed after considering specified precedence operations

--- 18/solution.rs
+++ 18/solution.rs
@@ -1,9 +1,8 @@
 use std::env;
 use std::fs;
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum Op {
-    Noop,
     Add,
     Multiply,
     OpenBrace,
@@ -13,7 +12,6 @@ enum Op {
 impl Op {
     fn apply(&self, acc: i64, val: i64) -> i64 {
         match self {
-            Op::Noop => val,
             Op::Add => acc + val,
             Op::Multiply => acc * val,
             Op::OpenBrace => panic!("Unsupported operation - apply {:?} to values {} and {}", self, acc, val),
@@ -23,42 +21,87 @@ impl Op {
 }
 
 struct Expression {
-    stack: Vec<(i64, Op)>, 
-    sub: i64,
-    op: Op,
+    raw: String,
+    precedence: Vec<Op>,
+    val_stack: Vec<i64>,
+    op_stack: Vec<Op>,
+    sub_stack: Vec<(Vec<i64>, Vec<Op>)>,
 }
 
 impl Expression {
-    fn new() -> Expression {
+    fn new(raw: String, precedence: Vec<Op>) -> Expression {
         Expression {
-            stack: vec![],
-            sub: 0,
-            op: Op::Noop,
+            raw,
+            precedence,
+            val_stack: vec![],
+            op_stack: vec![],
+            sub_stack: vec![],
         }
     }
 
+    fn process(&mut self) -> i64 {
+        for ch in self.raw.clone().chars() {
+            match ch {
+                '(' => self.apply_op(Op::OpenBrace),
+                ')' => self.apply_op(Op::CloseBrace),
+                '+' => self.apply_op(Op::Add),
+                '*' => self.apply_op(Op::Multiply),
+                _ => {
+                    let val = String::from(ch).parse::<i64>().unwrap();
+                    self.apply_val(val);
+                },
+            }
+        }
+
+        self.process_current_stacks()
+    }
+
     fn apply_val(&mut self, val: i64) {
-        self.sub = self.op.apply(self.sub, val);
-        self.op = Op::Noop;
+        self.val_stack.push(val);
     }
 
     fn apply_op(&mut self, op: Op) {
         match op {
             Op::OpenBrace => {
                 // println!("Push to stack {} {:?}", sub, op);
-                self.stack.push((self.sub, self.op));
-                self.sub = 0;
-                self.op = Op::Noop;
+                self.sub_stack.push((self.val_stack.clone(), self.op_stack.clone()));
+                self.val_stack = vec![];
+                self.op_stack = vec![];
             },
             Op::CloseBrace => {
-                let (prev_sub, prev_op) = self.stack.pop().unwrap();
+                let val = self.process_current_stacks();
+                let (prev_vals, prev_ops) = self.sub_stack.pop().unwrap();
                 // print!("Popped from stack {} {:?} with {} {:?} to ", prev_sub, prev_op, sub, op);
-                self.sub = prev_op.apply(prev_sub, self.sub);
-                // println!("{}", sub);
-                self.op = Op::Noop;
+                self.val_stack = prev_vals;
+                self.val_stack.push(val);
+                self.op_stack = prev_ops;
             },
-            _ => self.op = op,
+            _ => self.op_stack.push(op),
         }
+    }
+
+    fn process_current_stacks(&mut self) -> i64 {
+        // Should be able to set whatever precedence is desired for any future ops here
+        for op in self.precedence.iter() {
+            let mut i = 0;
+            while i < self.op_stack.len() {
+                if self.op_stack[i] == *op {
+                    self.val_stack[i] = op.apply(self.val_stack[i], self.val_stack[i + 1]);
+                    self.val_stack.remove(i + 1);
+                    self.op_stack.remove(i);
+                } else {
+                    i += 1;
+                }
+            }
+        }
+
+        while self.op_stack.len() > 0 {
+            self.val_stack[0] = self.op_stack[0].apply(self.val_stack[0], self.val_stack[1]);
+            self.val_stack.remove(1);
+            self.op_stack.remove(0);
+        }
+        
+        self.val_stack[0]
     }
 }
 
@@ -72,29 +115,28 @@ fn main() {
         .map(String::from)
         .map(|s| s.replace(" ", ""))
         .collect();
-        
+
+    // task 1
     let sum = input.iter()
         .map(|line| {
-            let mut expression = Expression::new(); 
-            // println!("{}", line);
-            for ch in line.chars() {
-                match ch {
-                    '(' => expression.apply_op(Op::OpenBrace),
-                    ')' => expression.apply_op(Op::CloseBrace),
-                    '+' => expression.apply_op(Op::Add),
-                    '*' => expression.apply_op(Op::Multiply),
-                    _ => {
-                        let val = String::from(ch).parse::<i64>().unwrap();
-                        expression.apply_val(val);
-                    },
-                }
-            }
-            // println!("{} = {}", line, sub);
-
-            expression.sub
+            let mut expression = Expression::new(line.to_owned(), vec![]); 
+            let res = expression.process();
+            // println!("{} = {}", expression.raw, res);
+            res
         })
         .sum::<i64>();
 
+    println!("Sum of all expressions with no precedence: {}", sum);
 
-    println!("Sum of all expressions: {}", sum);
+    // task 2
+    let sum = input.iter()
+        .map(|line| {
+            let mut expression = Expression::new(line.to_owned(), vec![Op::Add, Op::Multiply]); 
+            let res = expression.process();
+            // println!("{} = {}", expression.raw, res);
+            res
+        })
+        .sum::<i64>();
+
+    println!("Sum of all expressions with [add, multiply] precedence: {}", sum);
 }
