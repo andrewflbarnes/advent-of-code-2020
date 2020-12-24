51f3f3a7ad9ef7f2cd1be9d934988045afa5dd7a
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2020-12-24 04:48:23 +0000

    Add feasible solution for day 23 challenge task 2
    - Switch to n implementation where we just have a large vec where each
      index is the cup number and the value is the cup next to it. This
      dramatically optimises the "cut and move" as it just involves
      modifying 3 indexes

--- 23/src/main.rs
+++ 23/src/main.rs
@@ -2,84 +2,110 @@ fn main() {
     // let args: Vec<String> = env::args().collect();
     // let input = "389125467"; // test
     let input = "589174263";
+    // play_game(input, 10, false);
     play_game(input, 100, false);
     play_game(input, 10_000_000, true);
 
 }
 
 fn play_game(input: &str, iters: usize, big_game: bool) {
-    let mut cups: Vec<usize> = input.chars()
-        .map(String::from)
-        .map(|c| c.parse::<usize>().unwrap())
-        .collect();
-    let high_cup = *cups.iter().max().unwrap();
-
+    let mut cups: Vec<usize>;
     if big_game {
-        for i in (high_cup + 1)..=1_000_000 {
-            cups.push(i);
-        }
+        cups = vec![0; 1_000_001];
+        // cups = vec![0; 1_001];
+    } else {
+        cups = vec![0; input.len() + 1];
     }
 
+    let first_cup = input[0..1].parse::<usize>().unwrap();
+    let init = input.chars()
+        .map(String::from)
+        .map(|c| c.parse::<usize>().unwrap());
+    let mut last = init
+        .fold(0, |last, val| {
+            if last != 0 {
+                cups[last] = val;
+            }
+            val
+        });
+    
+    for i in (input.len() + 1)..cups.len() {
+        cups[last] = i;
+        last = i;
+    }
+
+    let high_cup = *cups.iter().max().unwrap();
+    cups[last] = first_cup;
+
+    // let mut cup = first_cup;
+    // for _ in 0..(cups.len() - 1) {
+    //     print!("Cup {} ", cup);
+    //     cup = cups[cup];
+    //     println!("points to {}", cup);
+    // }
+    // println!("From {} to {}", first_cup, cup);
+
     let mut buff = vec![];
-    let cup_count = cups.len();
 
-    let mut cup_idx = 0;
+    let mut cup_idx = first_cup;
     for its in 0..iters {
-        if its % 1000 == 0 {
-            println!("Completed {} iterations", its);
-        }
-        // println!("");
-        let cup = cups.get(cup_idx).unwrap().clone();
-        // println!("Current cup is {} at index {}\n({:?})", cup, cup_idx, cups);
-        let mut removal_idx = (cup_idx + 1) % cup_count;
-        for _ in 0..3 {
-            if removal_idx >= cups.len() {
-                removal_idx = 0;
-            }
-            if removal_idx < cup_idx {
-                cup_idx -= 1;
-            }
-            buff.push(cups.remove(removal_idx));
-        }
+        // clear check buffer
+        buff.clear();
 
-        let mut target_cup = cup;
+        // the number of the current cup
+        let current_cup = cup_idx;
+        // the number of the next cup (which we start the cut from)
+        let cut_start = cups[cup_idx];
+        buff.push(cut_start);
+        // the second cup we cut - no standalone variable name as we don't need it later outside the buff
+        cup_idx = cups[cut_start];
+        buff.push(cup_idx);
+        // the third and final cup we cut
+        let cut_end = cups[cup_idx];
+        buff.push(cut_end);
+        // the cup to use for the next iteration
+        cup_idx = cups[cut_end];
+        // println!("source: {}", current_cup);
+        // print!("cups:");
+        // let mut temp = first_cup;
+        // for _ in 1..cups.len() {
+        //     print!(" {}", temp);
+        //     temp = cups[temp];
+        // }
+        // println!("");
+        // println!("pick up: {:?}", buff);
+
+        let mut target_cup = current_cup;
         loop {
-            target_cup = if target_cup == 0 {
+            target_cup = if target_cup == 1 {
                 high_cup
             } else {
                 target_cup - 1
             };
 
-            if let Some(idx) = cups.iter().position(|&c| c == target_cup) {
-                if idx < cup_idx {
-                    cup_idx = (cup_idx + 3) % cup_count;
-                }
-                // println!("Pick up: {:?}", buff);
-                // println!("Destination: {} at index {}", target_cup, idx + 1);
-                cups.splice((idx + 1)..(idx + 1), buff.clone());
-                // cups.push(buff);
-                buff.clear();
-                // println!("After: {:?}", cups);
+            if !buff.contains(&target_cup) {
+                // println!("destination: {}\n", target_cup);
+                cups[current_cup] = cups[cut_end];
+                cups[cut_end] = cups[target_cup];
+                cups[target_cup] = cut_start;
                 break;
             }
         }
-        cup_idx = (cup_idx + 1) % cup_count;
     }
 
-    let one_idx = cups.iter().position(|&c| c == 1).unwrap();
+    
+    let after_one = cups[1];
     print!("Cup ring after {} rounds with ", iters);
-    if big_game {
+    if !big_game {
         print!("post 1 sequence: ");
-        for i in 1..cup_count {
-            print!("{}", cups.get((one_idx + i) % cup_count).unwrap());
+        let mut temp = after_one;
+        for _ in 2..cups.len() {
+            print!("{}", temp);
+            temp = cups[temp];
         }
         println!("");
     } else {
-        let next_cups = (
-            cups.get((one_idx + 1) % cup_count).unwrap(),
-            cups.get((one_idx + 2) % cup_count).unwrap()
-        );
-        print!("first 2 cups after 1 {}, {} product {}: ", next_cups.0, next_cups.1, next_cups.0 * next_cups.1);
+        print!("first 2 cups {}, {} product {}: ", after_one, cups[after_one], after_one * cups[after_one]);
 
     }
 }
