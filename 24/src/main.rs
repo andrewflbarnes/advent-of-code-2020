1d9801665bc0a669559c0d10763fe5163821ecf8
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2020-12-25 01:13:07 +0000

    Add day 24 challenge task 2 solution un Rust

--- 24/src/main.rs
+++ 24/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::collections::HashSet;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,42 +12,85 @@ fn main() {
         .map(String::from)
         .collect::<Vec<String>>();
     
-    let mut black_tiles = vec![];
+    let mut black_tiles: HashSet<(i32, i32)> = HashSet::new();
     instrs.iter().for_each(|i| {
         let mut instr = String::from("");
         let mut tile = (0, 0);
         i.chars().for_each(|c| {
             instr.push(c);
-            // print!("{:?} => ", tile);
             match &instr[..] {
-                "e" => tile.0 += 1,
-                "se" => tile.1 += 1,
+                "e" => tile.1 += 1,
+                "se" => tile.0 += 1,
                 "sw" => {
-                    tile.0 -= 1;
-                    tile.1 += 1;
-                },
-                "w" => tile.0 -= 1,
-                "nw" => tile.1 -= 1,
-                "ne" => {
                     tile.0 += 1;
                     tile.1 -= 1;
+                },
+                "w" => tile.1 -= 1,
+                "nw" => tile.0 -= 1,
+                "ne" => {
+                    tile.0 -= 1;
+                    tile.1 += 1;
                 },
                 "s" | "n" => return,
                 _ => panic!("{}", instr),
             }
-            // println!("{:?} ({})", tile, instr);
             instr.clear();
         });
 
-        // print!("{} {:?} => ", black_tiles.len(), tile);
         if black_tiles.contains(&tile) {
             black_tiles.retain(|&t| t != tile);
         } else {
-            black_tiles.push(tile);
+            black_tiles.insert(tile);
         }
-        // println!("{}", black_tiles.len());
     });
 
-    println!("Black tile count: {}", black_tiles.len());
-    // black_tiles.iter().for_each(|t| {println!("{:?}", t)});
+    let iters = 100;
+    let mut buff:HashSet<(i32, i32)> = HashSet::new();
+    println!("Day 0: {}", black_tiles.len());
+    for _ in 1..=iters {
+        buff.clear();
+        for tile in black_tiles.iter() {
+            let adj = count_adj(&black_tiles, tile);
+            if adj > 0 && adj <= 2 {
+                buff.insert(*tile);
+            }
+
+            push_if_black((tile.0, tile.1 + 1), &black_tiles, &mut buff);
+            push_if_black((tile.0, tile.1 - 1), &black_tiles, &mut buff);
+            push_if_black((tile.0 + 1, tile.1), &black_tiles, &mut buff);
+            push_if_black((tile.0 - 1, tile.1), &black_tiles, &mut buff);
+            push_if_black((tile.0 + 1, tile.1 - 1), &black_tiles, &mut buff);
+            push_if_black((tile.0 - 1, tile.1 + 1), &black_tiles, &mut buff);
+        }
+        black_tiles = buff.clone();
+    }
+    
+    println!("Day {}: {}", iters, black_tiles.len());
+}
+
+fn push_if_black( tile: (i32, i32), black_tiles: &HashSet<(i32, i32)>,buff: &mut HashSet<(i32, i32)>) {
+    if black_tiles.contains(&tile) || buff.contains(&tile) {
+        return;
+    }
+    let adj = count_adj(&black_tiles, &tile);
+    if black_tiles.contains(&tile) {
+        if adj > 0 && adj <= 2 {
+            buff.insert(tile);
+        }
+    } else {
+        if adj == 2 {
+            buff.insert(tile);
+        }
+    }
+}
+
+fn count_adj(black_tiles: &HashSet<(i32, i32)>, tile: &(i32, i32)) -> i32 {
+    let mut adj = 0;
+    adj += if black_tiles.contains(&(tile.0, tile.1 + 1)) {1} else {0};
+    adj += if black_tiles.contains(&(tile.0, tile.1 - 1)) {1} else {0};
+    adj += if black_tiles.contains(&(tile.0 + 1, tile.1)) {1} else {0};
+    adj += if black_tiles.contains(&(tile.0 - 1, tile.1)) {1} else {0};
+    adj += if black_tiles.contains(&(tile.0 + 1, tile.1 - 1)) {1} else {0};
+    adj += if black_tiles.contains(&(tile.0 - 1, tile.1 + 1)) {1} else {0};
+    adj
 }
